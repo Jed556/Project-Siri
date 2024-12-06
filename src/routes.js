112b7091b75a716ec86290aa924416c9751d3f14
Jed556 <jguiriba11@gmail.com>
2024-12-06 17:12:00 +0800

    fix: Fix typo

--- src/routes.js
+++ src/routes.js
@@ -26,7 +26,7 @@ import Tables from "layouts/tables";
 import Billing from "layouts/billing";
 import RTL from "layouts/rtl";
 import Notifications from "layouts/notifications";
-import WeeklyIteineary from "layouts/weekly-iteineary";
+import WeeklyIteineary from "layouts/weekly-itinerary";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
