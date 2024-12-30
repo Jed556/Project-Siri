c91b4c8ef807e9c77d86b34a7fa6c25b25b2c83f
Jed556 <jguiriba11@gmail.com>
2024-12-30 16:33:21 +0800

    fix: Ensure sheets are reloaded

--- src/utils/MasterSheetDb.js
+++ src/utils/MasterSheetDb.js
@@ -58,11 +58,13 @@ class MasterSheetDb {
     }
 
     async appendSheetData(name, type, dateCreated, creator, sheetId, isTrashed = false) {
+        this.loadSheets();
         this.sheets.push([name, type, dateCreated, creator, sheetId, isTrashed]);
         await this.saveSheets();
     }
 
     openSheetUrl(sheetId) {
+        this.loadSheets();
         const sheet = this.sheets.find((sheet) => sheet[4] === sheetId);
         if (sheet) {
             const url = `https://docs.google.com/spreadsheets/d/${sheetId}`;
@@ -73,6 +75,7 @@ class MasterSheetDb {
     }
 
     async markSheetAsTrashed(sheetId) {
+        this.loadSheets();
         const sheet = this.sheets.find((sheet) => sheet[4] === sheetId);
         if (sheet) {
             sheet[5] = true;
@@ -83,20 +86,24 @@ class MasterSheetDb {
     }
 
     getAllUntrashedSheets() {
+        this.loadSheets();
         return this.sheets.filter((sheet) => !sheet[5]);
     }
 
     getTrashedSheets() {
+        this.loadSheets();
         return this.sheets.filter((sheet) => sheet[5]);
     }
 
     getFilteredSheets(filters) {
+        this.loadSheets();
         return this.sheets.filter((sheet) => {
             return filters.every(([index, value]) => sheet[index] === value);
         });
     }
 
     exportToFile(filename = "MasterSheetDb.xlsx") {
+        this.loadSheets();
         const worksheet = utils.aoa_to_sheet(this.sheets);
         const workbook = utils.book_new();
         utils.book_append_sheet(workbook, worksheet, "MasterSheetDb");
