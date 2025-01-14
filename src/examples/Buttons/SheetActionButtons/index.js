a204ca9ee1b80b305ae9b9fbc1eac38da6d7a157
Jed556 <jguiriba11@gmail.com>
2025-01-14 14:04:46 +0800

    style: Update button text

--- src/examples/Buttons/SheetActionButtons/index.js
+++ src/examples/Buttons/SheetActionButtons/index.js
@@ -184,7 +184,7 @@ function SheetActionButtons({ data, readonly, onSheetChange }) {
 
     const getSelectedSpreadsheetName = () => {
         if (selectedSpreadsheet === "new") {
-            return "New Sheet";
+            return "History"; // New Sheet
         }
         const spreadsheet = spreadsheets.find(
             (spreadsheet) => spreadsheet[4] === selectedSpreadsheet
@@ -284,17 +284,15 @@ function SheetActionButtons({ data, readonly, onSheetChange }) {
                                     ),
                                 () => fetchSpreadsheets(setSpreadsheets, data.type, user[4], true),
                             ],
-                            `Spreadsheet ${
-                                selectedSpreadsheet === "new" ? "created" : "updated"
+                            `Spreadsheet ${selectedSpreadsheet === "new" ? "created" : "updated"
                             } successfully`,
-                            `Failed to ${
-                                selectedSpreadsheet === "new" ? "create" : "update"
+                            `Failed to ${selectedSpreadsheet === "new" ? "create" : "update"
                             } spreadsheet`
                         )
                     }
                     style={{ marginLeft: 10 }}
                 >
-                    {selectedSpreadsheet === "new" ? "New Google Sheet" : "Save Google Sheet"}{" "}
+                    {selectedSpreadsheet === "new" ? "Save Google Sheet" : "Save Google Sheet"}
                     {/* {selectedSpreadsheet === "new" ? "New Google Sheet" : "Save Google Sheet"} */}
                 </MDButton>
             )}
